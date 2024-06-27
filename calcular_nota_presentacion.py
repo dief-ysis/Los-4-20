99f4c972a0d01c4f6cab40e9b171b2160e21992b
diefysis <dav.fuentesr@duocuc.cl>
2024-06-27 00:17:36 -0400

    subiendo funcion calcular nota

---
+++ calcular_nota_presentacion.py
@@ -0,0 +1,8 @@
+def calcular_nota_presentacion(p1, p2, p3):
+    ponderacion_p1 = 0.30
+    ponderacion_p2 = 0.35
+    ponderacion_p3 = 0.35
+    
+    nota_presentacion = p1 * ponderacion_p1 + p2 * ponderacion_p2 + p3 * ponderacion_p3
+    
+    return nota_presentacion
